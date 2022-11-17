ee5c17d0ddc21936725d8628c8d5043e366a0691
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2022-11-17 11:51:24 -0300

    configuracion de la instancia de axios y maquetado de pedidos

--- pages/forms/index.js
+++ pages/forms/index.js
@@ -2,21 +2,25 @@
 // Columnas posibles: “nombre”, “cant. de respuestas”, “fecha de creación”.
 // Al clickear en una fila redirigir a /forms/[formID]/manage.
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import axios from "../../axios";
+
 import fakeForms from "../../utils/fakeForms";
 
+import {
+  Grid,
+  Button,
+  TableRow,
+  TablePagination,
+  TableHead,
+  TableContainer,
+  TableCell,
+  TableBody,
+  Table,
+  Paper,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import Button from "@material-ui/core/Button";
-import { Grid } from "@material-ui/core";
 
 const columns = [
   { id: "title", label: "Nombre", minWidth: 100 },
@@ -29,36 +33,22 @@ const columns = [
   },
 ];
 
-const rows = fakeForms();
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
-  head: {
-    backgroundColor: "#fff",
-    position: "sticky",
-    top: 0,
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    minWidth: 250,
-  },
-}));
+//const rows = fakeForms();
 
 export default function StickyHeadTable() {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(25);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState(fakeForms());
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+  /*useEffect(() => {
+    axios.get("/forms").then((res) => {
+      setRows(res).catch((err) => {
+        console.log(err);
+      });
+    });
+  }, []);*/
 
   return (
     <Paper className={classes.root}>
@@ -75,7 +65,6 @@ export default function StickyHeadTable() {
           Nuevo Formulario
         </Button>
       </Grid>
-
       <Table className={classes.head}>
         <TableHead>
           <TableRow>
@@ -120,14 +109,34 @@ export default function StickyHeadTable() {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[25, 100]}
+        rowsPerPageOptions={[10, 25, 100]}
         component="div"
         count={rows.length}
         rowsPerPage={rowsPerPage}
         page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+        onPageChange={(e, newPage) => {
+          setPage(newPage);
+        }}
+        onRowsPerPageChange={(e) => {
+          setRowsPerPage(+e.target.value);
+          setPage(0);
+        }}
       />
     </Paper>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  head: {
+    backgroundColor: "#fff",
+    position: "sticky",
+    top: 0,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    minWidth: 250,
+  },
+}));
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,16 @@ import React, { useEffect } from "react";
 import Router from "next/router";
 import Head from "next/head";
 
-import styles from "../styles/Home.module.css";
-import { Link } from "@material-ui/core";
-
 export default function Home() {
   useEffect(() => {
     Router.push("/login");
   });
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Formidable</title>
-        <meta name="description" content="Generador de formularios dinámicos" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/*  <main className={styles.main}></main>
-      <Link href="/login">Formidable</Link>
-      <footer className={styles.footer}></footer> */}
-    </div>
+    <Head>
+      <title>Formidable</title>
+      <meta name="description" content="Generador de formularios dinámicos" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   );
 }
